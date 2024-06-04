41387123b0fa6d6cf85a6526f7a39f8c1d2895ca
Siva <sivalives@gmail.com>
2024-06-04 13:44:16 +0100

    changes

--- samba/setup_samba_docker_volume.sh
+++ samba/setup_samba_docker_volume.sh
@@ -1,11 +1,12 @@
 #/bin/bash
-sudo docker volume create iptv-volume
-sudo docker pull trnape/rpi-samba
-if [ -z $1 ] || [ -z $2 ]; then
-  echo "pass samba server username and password you want to use"
-  exit
-else
-  docker stop docker-samba && docker rm -f docker-samba
-  sudo docker run -d -p 127.0.0.1:445:445 -v /var/lib/docker/volumes/iptv-volume/_data:/share/data --name docker-samba trnape/rpi-samba -u "$1:$2" -s "pi-files:/share/data/:rw:pi"
-fi 
-sudo docker update --restart unless-stopped $(docker ps -q)
+echo "Use docker-compose to setup samba , may remove this script - check on volumes part"
+#sudo docker volume create iptv-volume
+#sudo docker pull trnape/rpi-samba
+#if [ -z $1 ] || [ -z $2 ]; then
+#  echo "pass samba server username and password you want to use"
+#  exit
+#else
+#  docker stop docker-samba && docker rm -f docker-samba
+#  sudo docker run -d -p 127.0.0.1:445:445 -v /var/lib/docker/volumes/iptv-volume/_data:/share/data --name docker-samba trnape/rpi-samba -u "$1:$2" -s "pi-files:/share/data/:rw:pi"
+#fi 
+#sudo docker update --restart unless-stopped $(docker ps -q)
