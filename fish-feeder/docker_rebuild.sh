c55ace2c6f273441deb0d6c4ea1847194c8579fb
Siva <sivalives@gmail.com>
2024-06-04 06:06:07 +0100

    fish feeder working

--- fish-feeder/docker_rebuild.sh
+++ fish-feeder/docker_rebuild.sh
@@ -13,8 +13,7 @@ docker image rm fish-feeder
 docker build -t fish-feeder .
 
 #Run and create container
-#docker run -d --restart unless-stopped --name fish-feeder fish-feeder
-docker run -d -v $(pwd)/trigger-motor.sh:/home/trigger-motor.sh  --restart unless-stopped --name fish-feeder fish-feeder
+docker run --privileged -d --restart unless-stopped --name fish-feeder fish-feeder 
 
 #Login to Docker Container : 
 #docker exec -it fish-feeder bash
--- fish-feeder/trigger-motor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#trigger to start fish feeding
-
-python3 stepper_motor.py
